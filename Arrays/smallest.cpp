55d82f3710523a7eec6e889af2cbdc558a6680bc
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-20 09:39:47 +0000

    Find the smallest element in an array

--- Arrays/smallest.cpp
+++ Arrays/smallest.cpp
@@ -2,24 +2,34 @@
 #include <bits/stdc++.h>
  
 using namespace std;
-int SmallestElement(int arr[], int n) {
+int SmallestElement(int number[], int size) {
  
-  int min = arr[0];
-  for (int i = 0; i < n; i++) {
-    if (min > arr[i]) {
-      min = arr[i];
+  int min = number[0];
+  for (int i = 0; i < size; i++) {
+    if (min > number[i]) {
+      min = number[i];
     }
   }
   return min;
 }
 int main() {
-  int arr1[] = {2,5,1,3,0};
-  int n = 5;
-  int min = SmallestElement(arr1, n);
+  int size;
+  cin>>size;
+int number[size];
+  for(int i=0;i<size;i++){
+    cin>>number[i];
+int number[i];
+  }
+  for(int i=0;i<size;i++){
+    cout<<number[i];
+  }
+ // int arr1[] = {2,5,1,3,0};
+  //int n = 5;
+  int min = SmallestElement(number, size);
   cout << "The smallest element in the array is: " << min << endl;
  
   int arr2[] =  {8,10,9,7,9};
-  n = 5;
+  int n = 5;
   min = SmallestElement(arr2, n);
   cout << "The smallest element in the array is: " << min;
   return 0;
