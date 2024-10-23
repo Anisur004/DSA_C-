10120eda513d1c308a52fe8a646a7ce3a5783894
Anisur004 <167961544+Anisur004@users.noreply.github.com>
2024-10-23 05:46:57 +0000

    Find Smallest element in array

--- Arrays/smallest.cpp
+++ Arrays/smallest.cpp
@@ -1,37 +1,49 @@
-#include <iostream>
+// **************** using min variable*******************
+//#include <iostream>
+// #include <bits/stdc++.h>
+ 
+// using namespace std;
+// int SmallestElement(int number[], int size) {
+ 
+//   int min = number[0];
+//   for (int i = 0; i < size; i++) {
+//     if (min > number[i]) {
+//       min = number[i];
+//     }
+//   }
+//   return min;
+// }
+
+// int main(){
+//   int n;
+//   cin>>n;
+//   int arr[n];
+//   for(int i=0;i<n;i++)
+//   {
+//     cin>>arr[i];
+//   }
+//   for(int i=0;i<n;i++){
+//     cout<<arr[i];
+
+//   }
+//   int min=SmallestElement(arr,n);
+//   cout<<"The smallest element is"<<min<<endl;
+  
+//  int arr2[]={3,5,7,8,9};
+//  int num=5;
+//     min=SmallestElement(arr2,num);
+//   cout<<"The smallest element is"<<min;
+//   return 0;
+// }
+// using sorting
 #include <bits/stdc++.h>
- 
-using namespace std;
-int SmallestElement(int number[], int size) {
- 
-  int min = number[0];
-  for (int i = 0; i < size; i++) {
-    if (min > number[i]) {
-      min = number[i];
-    }
-  }
-  return min;
-}
-int main() {
-  int size;
-  cin>>size;
-int number[size];
-  for(int i=0;i<size;i++){
-    cin>>number[i];
-int number[i];
-  }
-  for(int i=0;i<size;i++){
-    cout<<number[i];
-  }
- // int arr1[] = {2,5,1,3,0};
-  //int n = 5;
-  int min = SmallestElement(number, size);
-  cout << "The smallest element in the array is: " << min << endl;
- 
-  int arr2[] =  {8,10,9,7,9};
-  int n = 5;
-  min = SmallestElement(arr2, n);
-  cout << "The smallest element in the array is: " << min;
+ using namespace std;
+ int sortArray(vector <int>& arr){
+  sort(arr.begin(),arr.end());
+  return arr[0];
+ }
+ int main(){
+  vector<int> arr1={7,5,3,8,9};
+  cout<<"Smallest element is"<<sortArray(arr1);
   return 0;
-}
- 
+ }
